00fa42cede9681fb0742089f614d51aa9c7c2398
Davis Foote <djfoote@berkeley.edu>
2013-11-23 18:18:42 -0800

    First commit. Improv.py selects a scale based on a chord name input.

---
+++ improv.py
@@ -0,0 +1,19 @@
+"""
+"""
+
+from mingus.core import progressions, intervals, chords
+from mingus.containers import NoteContainer, Note
+from mingus.midi import fluidsynth
+import time, sys
+from random import random
+
+choose_scale = {'M' : ionian,
+				'm' : aeolian,
+				'o' : locrian,
+				'+' : whole_note,
+				'7' : mixolydian,
+				'm7': aeolian,
+				'M7': ionian}
+
+def generate_scale(chord_name):
+	return choose_scale[chord_name[1:]](chord_name[:1])
